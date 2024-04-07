6e0312dfc5a8f1243c97103af365bda772265c76
dolphin <dosometh@mail.com>
2024-04-07 20:20:19 +1000

    first task

--- tasks/is_prime.py
+++ tasks/is_prime.py
@@ -1,15 +1,17 @@
-__all__ = ("is_prime",)
+__all__ = (
+    'seconds_to_str',
+)
+from datetime import datetime
 
-
-def is_prime(number: int) -> bool:
-    """Определяет, является ли число простым.
-
-    Example:
-        >> is_prime(0):
-        False
-        >> is_prime(1):
-        False
-        >> is_prime(4):
-        True
-    """
-    raise NotImplementedError
+def seconds_to_str(seconds: int) -> str:
+   m, s = divmod(seconds, 60)
+   h, m = divmod(m, 60)
+   d, h = divmod(h, 24)
+   if seconds < 60:
+       return f'{s:02d}s'
+   elif seconds < 3600:
+       return f'{m:02d}m{s:02d}s'
+   elif seconds < 86400:
+       return f'{h:02d}h{m:02d}m{s:02d}s'
+   else:
+       return f'{d:02d}d{h:02d}h{m:02d}m{s:02d}s'
